007fb7a53fffc8646658925787656af531a7a5bc
takumabo <kingcongyatuhashi.1043@gmail.com>
2019-06-04 17:31:44 +0800

    問１
    作業終了しました。

--- index.php
+++ index.php
@@ -2,28 +2,54 @@
 
 //演習問題1
 
-
+//インストラクタ
 $kid = new Kid();
 
-$kid->setShowName();
+//個別に出力する
+// $kid->setName('');
+// $kid->setSex('');
+
 
 class  Kid{
     //プロパティ
     private $name;
-    // private $sex;
+    private $sex;
 
+    // 初期値設定(コンストラクタ)
     function __construct(){
         $this->name = 'Seed';
-        // $this->sex = 'boy'
-    }
-    //メソッド
-    function setShowName($aaa){
-        $this->name = $aaa;
-        // $this->sex = $aaa
+        $this->sex = 'boy';
     }
 
-    function getShowName(){
+
+    //メソッド(name)
+    public function setName($aaa){
+    $this->name = $aaa;
+  }
+    public function getName(){
         return $this->name;
-        // return $this->sex;
+    }
+
+    //メソッド(sex)
+    public function setSex($bbb){
+        $this->sex = $bbb;
+    }
+    public function getSax(){
+        return $this->sex;
+    }
+
+
+    //性別に応じて出力を変えるメソッド
+    public function showName(){
+        if($this->sex == 'boy'){
+            return $this->name .' くん';
+        } elseif($this->sex == 'girl') {
+            return $this->name . ' ちゃん';
+        } else {
+            return $this->name .'不明';
+        }
     }
 }
+
+//最終出力部分
+echo $kid->showName();
