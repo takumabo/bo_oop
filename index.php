e61dcc3b895526e209c4080db2e4308cdd4135fa
takumabo <kingcongyatuhashi.1043@gmail.com>
2019-06-04 15:42:41 +0800

    問１作業
    作業途中。

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+
+//演習問題1
+
+$kid = new Kid();
+
+$kid->setShowName();
+
+class  Kid{
+    //プロパティ
+    private $name;
+    // private $sex;
+
+    function __construct(){
+        $this->name = 'Seed';
+        // $this->sex = 'boy'
+    }
+    //メソッド
+    function setShowName($aaa){
+        $this->name = $aaa;
+        // $this->sex = $aaa
+    }
+
+    function getShowName(){
+        return $this->name;
+        // return $this->sex;
+    }
+}
